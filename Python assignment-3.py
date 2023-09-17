68a3855a7f37951bab269ace386e65c5f75b82b6
Abhinav2345 <145286150+Abhinav2345@users.noreply.github.com>
2023-09-18 00:08:22 +0530

    Update Python assignment-3.py

--- Python assignment-3.py
+++ Python assignment-3.py
@@ -4,15 +4,7 @@ def reverse(s):
     for i in s:
         str = i + str
     return str
-str_g='my name is ansh'
+str_g='my name is abhi'
 print(str_g)
 print(reverse(str_g))
-# Write a program to print a given string in reverse order.
-def reverse(s):
-    str= " "
-    for i in s:
-        str = i + str
-    return str
-str_g='my name is ansh'
-print(str_g)
-print(reverse(str_g))
+
